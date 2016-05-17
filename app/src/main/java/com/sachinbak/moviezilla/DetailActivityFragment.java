a1c78f113fc218e8b4514499440749aa077b11c6
Sachin Bakshi <sbakshi36@gmail.com>
2016-05-17 19:12:31 +0530

    Removing API Key

--- app/src/main/java/com/sachinbak/moviezilla/DetailActivityFragment.java
+++ app/src/main/java/com/sachinbak/moviezilla/DetailActivityFragment.java
@@ -31,7 +31,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.sachinbak.moviezilla.data.DataStore;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
